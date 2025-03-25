4a2fc1fc4fe05694dde03b3cf16feef8b56bf724
Davit <davit.ners02@gmail.com>
2025-03-25 10:15:36 +0100

    Add victory check funtions

---
+++ services/check-victory.ts
@@ -0,0 +1,51 @@
+const mockData: number[][] = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+];
+
+function checkRow(grid: number[][], player: number): boolean {
+    let victory: boolean;
+    for (const row of grid) {
+        victory = true;
+        for (const block of row) {
+            if (block !== player) victory = false;
+        }
+        if (victory) return true;
+    }
+    return false;
+};
+
+function checkCol(grid: number[][], player: number): boolean {
+    let victory: boolean;
+
+    for (let i = 0; i < grid.length; i++) {
+        victory = true;
+        for (let j = 0; j < grid[i].length; j++) {
+            if (grid[j][i] !== player) victory = false;
+        }
+        if (victory) return true;
+    }
+    return false;
+};
+
+function checkDiag(grid: number[][], player: number): boolean {
+    let victory = true;
+    for (let i = 0; i < grid.length; i++)  {
+        if (grid[i][i] !== player) victory = false;
+    }
+    if (victory) return true;
+    victory = true;
+
+    for (let i = 0; i < grid.length; i++) {
+        if (grid[i][grid.length - 1 - i] !== player) {
+            victory = false;
+            break;
+        }
+    }
+    return victory;
+};
+
+export default function checkVictory(grid: number[][], player: number): boolean {
+    return checkRow(grid, player) || checkCol(grid, player) || checkDiag(grid, player);
+};
